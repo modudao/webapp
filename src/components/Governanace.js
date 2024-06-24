4fd62db49da6dc66f88d05ad6fce07e70a2ef69a
solidity12 <volibear1001@gmail.com>
2024-06-25 01:06:36 +0900

    feat: Add join function

--- src/components/Governanace.js
+++ src/components/Governanace.js
@@ -22,9 +22,14 @@ function Governanace() {
   const [voteData, setVoteData] = useState("");
   const [voteRate, setVoteRate] = useState(0);
   const [winImage, setWinImage] = useState([]);
+  const [treasuryBalance, setTreasuryBalance] = useState(0);
+  const [winnerNickname, setWinnerNickname] = useState("");
+  const [joinRate, setJoinRate] = useState(0);
+  const [winnerAmount, setWinnerAmount] = useState(0);
 
   const nftAddress = "0xb065C2E339Ec555aA03EA5695939708673A9bb15";
   const voteAbi = '{"inputs": [{"internalType": "uint256","name": "option","type": "uint256"}],"name": "vote","outputs": [],"stateMutability": "nonpayable","type": "function"}';
+  const joinAbi = '{"inputs": [],"name": "join","outputs": [],"stateMutability": "nonpayable","type": "function"}';
   const nftAbi = [{
     "inputs": [],
     "name": "hasVotedGovernance1",
@@ -74,7 +79,43 @@ function Governanace() {
     ],
     "stateMutability": "view",
     "type": "function"
-  },];
+  }, {
+    "inputs": [],
+    "name": "getWinnerNickname",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "inputs": [],
+    "name": "participantCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "inputs": [],
+    "name": "winnerAmount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }];
 
   ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -87,6 +128,8 @@ function Governanace() {
     const checkVotedStatus = async () => {
       try {
         const userAdderss = localStorage.getItem('klipAddress');
+        setTreasuryBalance((await provider.getBalance(nftAddress)).toString() / 10 ** 18);
+
         if (userAdderss == '') {
           setHasVotedGovernance1(false);
         } else {
@@ -105,12 +148,26 @@ function Governanace() {
           } else {
             setHasVotedGovernance1(false);
           }
+
+          const govStatus2 = await nftContract.hasVotedGovernance2();
+          if (govStatus2) {
+            setHasVotedGovernance2(true);
+
+            setWinnerNickname(await nftContract.getWinnerNickname());
+            const totalCount = Number(await nftContract.tokenCounter());
+            const sum = Number(await nftContract.participantCount());
+            setJoinRate(Math.round(sum / totalCount * 100));
+            setWinnerAmount((await nftContract.winnerAmount()).toString() / 10 ** 18);
+          } else {
+            setHasVotedGovernance2(false);
+          }
         }
       } catch (error) {
         console.error('Error checking membership status:', error);
       }
     };
 
+    checkVotedStatus();
     // 10초마다 갱신
     const intervalId = setInterval(checkVotedStatus, 10000);
 
@@ -178,6 +235,54 @@ function Governanace() {
       abi: voteAbi,
       value: '0',
       params,
+      from: localStorage.getItem('klipAddress'),
+    };
+
+    // Step 2: Request the contract action through Klip
+    const { request_key } = await prepare.executeContract(transaction);
+    // const res = await axios.post(A2P_API_PREPARE_URL, {
+    //   bapp: { name: bappName, },
+    //   transaction,
+    //   type: "execute_contract",
+    // })
+    // const { request_key } = res.data;
+
+    const userAgent = navigator.userAgent;
+    if (/Windows/i.test(userAgent) || /Macintosh/i.test(userAgent)) {
+      const qrURL = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+      // window.open(qrURL, '_blank');
+      setQrValue(qrURL);
+
+      openModal();
+    } else {
+      request(request_key);
+    }
+
+    // Step 3: Poll for the result
+    // const interval = setInterval(() => {
+    //   getResult(request_key, (result) => {
+    //     if (result.err) {
+    //       console.error(result.err);
+    //       clearInterval(interval);
+    //       return;
+    //     }
+    //     if (result.result) {
+    //       clearInterval(interval);
+    //     }
+    //   });
+    // }, 1000);
+  };
+
+  const join = async () => {
+    const bappName = 'MODULAB DAPP';
+
+    // Step 1: Prepare the contract action
+    const transaction = {
+      bappName,
+      to: nftAddress,
+      abi: joinAbi,
+      value: '0',
+      params: '[]',
       from: localStorage.getItem('klipAddress'),
     };
 
@@ -301,19 +406,26 @@ function Governanace() {
               <div className='voted-wrapper'>
                 <div className='gov-title-sub'>트레져리 물량을 분배해드립니다</div>
                 <div className='gov-title-sub2'>참여 안하신분들은 맴버쉽 구매에 사용하셨던 KLAY 그대로 돌려드립니다! <br /> <span style={{ fontWeight: 'bold' }}>참여한 사람 기준으로 컨트렉트 내에서 랜덤한 한명을 선택</span> 후 남은 KLAY 를 전송해드립니다! 랜덤한 사람을 추출하는 방식은 코드에 반영되어있어, 매우 투명하게 진행될꺼에요! </div>
-                <div className='gov-title-sub3'>*트레져리 물량: 3,000 KLAY</div>
-                <button className='gov-apply-button'>
+                <div className='gov-title-sub3'>*트레져리 물량: {treasuryBalance} KLAY</div>
+                <button className='gov-apply-button' onClick={join}>
                   <div className='gov-image-select-text'>참여</div>
                 </button>
+                <Modal
+                  isOpen={modalIsOpen}
+                  onRequestClose={closeModal}
+                  className="modal-content"
+                  overlayClassName="modal-overlay"
+                  contentLabel="QR Code Modal"
+                >
+                  <div className='membership-body-text'>QR 코드를 스캔하세요</div>
+                  <QRCode value={qrValue} size={256} />
+                </Modal>
               </div>
             ) : (
               <div className='voted-wrapper'>
-                <div className='gov-title-sub'>MG12님, 우승 축하드립니다</div>
-                <div className='gov-title-sub2'>받기 버튼을 눌러 100 KLAY 를 받아가세요!</div>
-                <div className='gov-title-sub3'>*참여율: 92%</div>
-                <button className='gov-apply-button'>
-                  <div className='gov-image-select-text'>받기</div>
-                </button>
+                <div className='gov-title-sub'>{winnerNickname} 님, 우승 축하드립니다</div>
+                <div className='gov-title-sub2'>다들 고생많으셨습니다! {winnerAmount} KLAY 멋지게 활용하시길 바라며, 다시 한 번 진심으로 축하드립니다!</div>
+                <div className='gov-title-sub3'>*참여율: {joinRate}%</div>
               </div>
             )}
           </div>
